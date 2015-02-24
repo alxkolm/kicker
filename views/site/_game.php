b85a4aa994842081208fa0dd9dac7af9b5fe326b
Alexey Kolmakov <alxkolm@bk.ru>
2015-02-24 19:47:00 +0300

    правильная отработка не существующих игроков +

--- views/site/_game.php
+++ views/site/_game.php
@@ -8,13 +8,13 @@ use app\models\Game;
 <div class="row game-item">
     <div class="col-md-5">
         <div><?= $model->playerA->lastname ?></div>
-        <div><?= $model->playerB->lastname ?></div>
+        <div><?= $model->playerB ? $model->playerB->lastname  : ''?></div>
     </div>
     <div class="col-md-2">
         <?= $model->scoreA ?>-<?= $model->scoreB ?>
     </div>
     <div class="col-md-5">
         <div><?= $model->playerC->lastname ?></div>
-        <div><?= $model->playerD->lastname ?></div>
+        <div><?= $model->playerD ? $model->playerD->lastname : '' ?></div>
     </div>
 </div>
